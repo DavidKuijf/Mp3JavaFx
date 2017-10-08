0e10425a5239e430d23b57e62f2e92cfa8d898c2
davic <Davici@live.nl>
2017-10-08 15:17:25 +0200

    forgot to comment the player

--- Mp3JavaFx/src/application/Mp3Player.java
+++ Mp3JavaFx/src/application/Mp3Player.java
@@ -7,11 +7,11 @@ import javafx.scene.media.MediaPlayer;
 public class Mp3Player {
 	
 	static void PlayingAnMp3(File mp3File) {
-		String mp3Path = mp3File.getAbsolutePath();
-		mp3Path = mp3Path.replace("\\", "/");
-		Media mp3 = new Media(new File(mp3Path).toURI().toString());
-		MediaPlayer player = new MediaPlayer(mp3);
-		player.play();
+		String mp3Path = mp3File.getAbsolutePath(); 					//get the path of the passed File
+		mp3Path = mp3Path.replace("\\", "/");							//Replace all \\ with /
+		Media mp3 = new Media(new File(mp3Path).toURI().toString());	//Make it readable for the player
+		MediaPlayer player = new MediaPlayer(mp3);						//Make the actual player instance
+		player.play();													//Start playing
 		
 		}
 
